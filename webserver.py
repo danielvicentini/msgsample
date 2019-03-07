3b7041efc33dd92d4e7dbc37b6c6b08c19ce47d1
danielvicentini <34514421+danielvicentini@users.noreply.github.com>
2019-03-07 18:09:41 -0300

    Add files via upload

--- webserver.py
+++ webserver.py
@@ -29,6 +29,7 @@ botmail="infobot@sparkbot.io"
 #########################################################
 ## FUNCOES
 
+### WEBEX TEAMS
 
 def CriaWebhook(webhook_name,webhook_url):
 
@@ -189,7 +190,171 @@ def webexmsgAll(msg):
 	return
 
 
+### OPERATIONS INSIGHTS
+
+def OpiCategorySearch(textosearch):
+
+	# Faz uma pesquisa no Operation Insights por Categorias existentes
+
+	# Parte 1 - pede Token para o OPI
+	url = "https://opinsights.cisco.com/api/am/v1/auth/license/accesstoken"
+	headers = {'Content-type': "application/json" , 'Authorization':'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnRJZCI6MjExLCJ0eXBlIjoiODQzMDc0YjAtMzIwYS0xMWU4LTg0YTctMzkzZWNiNTViY2Q5IiwidXNlck5hbWUiOiJmbGNvcnJlYUBjaXNjby5jb20iLCJpYXQiOjE1NTA4NjI5NTQsImp0aSI6IjQ2YjRiNTIwLTM2ZDYtMTFlOS04OTRmLTI3YTM3MDQ2NWExNCJ9.sNEnOPr45NXNRgx9AyEqU4x9xrt8ra-miRSX2rfH9K4'   }
+
+	# response = valor HTTP (nao usado ainda) e conteudo e' o conteudo de fato, covertendo em json
+	response = requests.request("GET", url, headers=headers)
+	conteudo=json.loads(response.content)
+	# resultado do token
+	token='JWT ' + str(conteudo['token'])
+
+	# Parte 2 - Consulta assets usando o Token
+	url = "https://opinsights.cisco.com/api/am/v1/entities/access/categories"
+	headers = {     'Content-type': "application/json" , 'Authorization': ''+token  }
+
+	# response = valor HTTP (nao usado ainda) e conteudo e' o conteudo de fato, covertendo em json
+	response = requests.request("GET", url, headers=headers)
+	Jdata=json.loads(response.content)
+
+	# Laco que Faz a pesquisa baseado no grupo do dispositivo da base acima
+
+	# Permite procurar tudo caso esta keyword seja usada
+	if textosearch == "tudo":
+		textosearch = ""
+
+	resultado = ""
+	
+	count = 0
+
+	for items in Jdata:
+
+	  msg=""
+
+	  if textosearch in str(items['department']['name']).lower() or textosearch in str(items['name']).lower():
+           
+		   #constroi saida de texto
+			 msg=msg+str("Nome:"+str(items['name'])+". Departamento: "+str(items['department']['name'])+"\n")
+			 count=count+1
+			 resultado = resultado + msg
+		
+	resultado = resultado + "\n"+str(count)+" Categorias Encontradas"	
+
+	return resultado
+
+
+def OpiAssetDetail(textosearch):
+	
+	# Pesquisa detalhes de um asset
+
+	# Parte 1 - pede Token para o OPI
+	url = "https://opinsights.cisco.com/api/am/v1/auth/license/accesstoken"
+	headers = {'Content-type': "application/json" , 'Authorization':'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnRJZCI6MjExLCJ0eXBlIjoiODQzMDc0YjAtMzIwYS0xMWU4LTg0YTctMzkzZWNiNTViY2Q5IiwidXNlck5hbWUiOiJmbGNvcnJlYUBjaXNjby5jb20iLCJpYXQiOjE1NTA4NjI5NTQsImp0aSI6IjQ2YjRiNTIwLTM2ZDYtMTFlOS04OTRmLTI3YTM3MDQ2NWExNCJ9.sNEnOPr45NXNRgx9AyEqU4x9xrt8ra-miRSX2rfH9K4'   }
+
+	# response = valor HTTP (nao usado ainda) e conteudo e' o conteudo de fato, covertendo em json
+	response = requests.request("GET", url, headers=headers)
+	conteudo=json.loads(response.content)
+	# resultado do token
+	token='JWT ' + str(conteudo['token'])
+
+	# Parte 2 - Consulta assets usando o Token
+	url = "https://opinsights.cisco.com/api/am/v1/entities/access/assets"
+	headers = {     'Content-type': "application/json" , 'Authorization': ''+token  }
+
+	# response = valor HTTP (nao usado ainda) e conteudo e' o conteudo de fato, covertendo em json
+	response = requests.request("GET", url, headers=headers)
+	Jdata=json.loads(response.content)
+
+
+	# Laco que Faz a pesquisa baseado no grupo do dispositivo da base acima
+
+	# Permite procurar tudo caso esta keyword seja usada
+	
+	msg="Nenhum resultado encontrado"
+
+	for items in Jdata:	
+		 
+		if textosearch in str(items['serial']).lower():
+
+			msg=""		
+			# Caso positivo encontrado monta a resposta
+			msg=msg+str("Asset:"+str(items['serial'])+"\n")	
+			msg=msg+str("Serial: "+str(items['tags'][0]['serial'])+"\n")
+			msg=msg+str("Tipo: "+str(items['tags'][0]['type'])+"\n")
+			msg=msg+str("Categoria: "+str(items['category']['name'])+"\n")
+			try:
+			 zona=str(items['location']['zones'][0]['name'])
+			 msg=msg+str("Local: Sala "+str(zona)+"\n")
+			except:
+			 pass
+			 msg=msg+str("Local não encontrado\n")		
+			
+	resultado=str(msg)			 
+
+	return resultado
+
+
+
+def OpiFindAssets(textosearch):
+	# Faz uma pesquisa no Operation Insights
+
+	# Parte 1 - pede Token para o OPI
+	url = "https://opinsights.cisco.com/api/am/v1/auth/license/accesstoken"
+	headers = {'Content-type': "application/json" , 'Authorization':'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnRJZCI6MjExLCJ0eXBlIjoiODQzMDc0YjAtMzIwYS0xMWU4LTg0YTctMzkzZWNiNTViY2Q5IiwidXNlck5hbWUiOiJmbGNvcnJlYUBjaXNjby5jb20iLCJpYXQiOjE1NTA4NjI5NTQsImp0aSI6IjQ2YjRiNTIwLTM2ZDYtMTFlOS04OTRmLTI3YTM3MDQ2NWExNCJ9.sNEnOPr45NXNRgx9AyEqU4x9xrt8ra-miRSX2rfH9K4'   }
+
+	# response = valor HTTP (nao usado ainda) e conteudo e' o conteudo de fato, covertendo em json
+	response = requests.request("GET", url, headers=headers)
+	conteudo=json.loads(response.content)
+	# resultado do token
+	token='JWT ' + str(conteudo['token'])
+
+	# Parte 2 - Consulta assets usando o Token
+	url = "https://opinsights.cisco.com/api/am/v1/entities/access/assets"
+	headers = {     'Content-type': "application/json" , 'Authorization': ''+token  }
+
+	# response = valor HTTP (nao usado ainda) e conteudo e' o conteudo de fato, covertendo em json
+	response = requests.request("GET", url, headers=headers)
+	Jdata=json.loads(response.content)
+
+
+	# Laco que Faz a pesquisa baseado no grupo do dispositivo da base acima
+
+	# Permite procurar tudo caso esta keyword seja usada
+	if textosearch == "tudo":
+		textosearch = ""
+
+	resultado = ""
+	
+	count = 0
+
+	for items in Jdata:
+
+		msg=""
+		 
+		if textosearch in str(items['tags'][0]['type']).lower() or textosearch in str(items['category']['name']).lower() or textosearch in str(items['serial']).lower():
+		
+			count = count +1
+						
+			# Caso encontrado monta a resposta
+			msg=msg+str(str(count)+")Asset:"+str(items['serial'])+" Categoria: "+str(items['category']['name'])+" . ")	
+			
+			try:
+			 zona=str(items['location']['zones'][0]['name'])
+			 msg=msg+str("Local: Sala "+str(zona)+"\n")
+			except:
+
+			 pass
+			 msg=msg+str("Local não encontrado\n")		
+			
+
+		
+		resultado = resultado + str(msg)
+
+		
+	resultado = resultado + "\n"+str(count)+" Assets Encontrados"	
+
+	return resultado
+
+
 def procura(textosearch):
+	# DEPRECADO - NAO USADO MAIS
 	# Faz uma pesquisa no Operation Insights
 
 	# Parte 1 - pede Token para o OPI
@@ -271,13 +436,24 @@ def webextalk(msg_id):
 
     # chamadas de acordo com os parametros
     if box == "ajuda":
-        msg="Chatops Teams 1.0\nComandos disponiveis:\nhelp: esta ajuda\nprocura <item>: Procurar Asset\n"
-        msg=msg+str("userid <email>: Identifica ID do usuario\nroomid <nome da sala>: Identifica ID da sala\nsalas: lista salas que pertenco\nexit: sai")
+        msg="Chatops Teams 1.0\nComandos disponiveis:\nhelp: esta ajuda\nProcura <nome>: Procurar local do Asset\nCategorias <nome ou tudo>: Lista Categorias cadastradas no OPI como nome identificado\nAsset <nome do asset>:apresenta detalhes do Asset\n"
+        msg=msg+str("userid <email>: Identifica ID do usuario\nroomid <nome da sala>: Identifica ID da sala\nsalas: lista salas que pertenco\n")
             
     # chama funcao para procurar OPI somente se ouver 2 parametros
     if box == "procura" and len(sp)>1:
         asset=sp[1]
-        msg = procura(asset)
+        msg = OpiFindAssets(asset)
+
+    # chama funcao para procurar OPI somente se ouver 2 parametros
+    if box == "categorias" and len(sp)>1:
+        categoria=sp[1]
+        msg = OpiCategorySearch(categoria)
+
+	# chama funcao para procurar OPI somente se ouver 2 parametros
+    if box == "asset" and len(sp)>1:
+        asset=sp[1]
+        msg = OpiAssetDetail(asset)
+ 
             
     # chamada funcao se houver no minimo 2 parametros
     if box == "userid" and len(sp)>1:
@@ -294,6 +470,8 @@ def webextalk(msg_id):
         
     # apos montagem da resposta em msg, envia a respectiva sala teams:
     webexmsgRoomviaID(idsala,msg)
+
+    return
 
 # RASCUNHOS ANTERIORES
 #########################################################
@@ -385,7 +563,7 @@ class S(BaseHTTPRequestHandler):
         # Conteudo
         content=json.loads(post_data.decode('utf-8'))
 		
-		# Msg de boas vindas para novas salas criadas
+		# Msg de boas vindas para novas salas criadas Não esta' funcionando
         if content['name']==webhook_name+"-new" and content['data']['personEmail']!=botmail:
             novasala=(content['data']['roomId'])
             webexmsgRoomviaID(novasala,"Olá! Digite ajuda para conhecer as opções.")
